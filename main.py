634660d00001ec5e99809db8ad6d45b0eaa864ae
摸鱼AI衔尾蛇 <113227682+SnakeSwallowingTail@users.noreply.github.com>
2022-09-11 15:46:55 +0800

    完善注释以及对部分内容进行修改

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ import jieba
 # 从路径获取文本内容
 def get_content(path):
     str = ''
-    f = open(path, 'r', encoding='UTF-8')
-    line = f.readline()
+    f = open(path, 'r', encoding='UTF-8')  # 打开文件
+    line = f.readline()  # 读取当前行并下移光标
     while line:
-        str = str + line
-        line = f.readline()
+        str = str + line  # 拼接文本
+        line = f.readline()  # 读取当前行并下移光标
     f.close()
-    return str
+    return str  # 返回读取后的文本并保存在内存中
 
 
 # jieba分词并过滤标点符号等
 def filter(str):
-    str = jieba.lcut(str)  # 直接返回list类型
+    str = jieba.lcut(str)  # jieba分词，直接返回list类型
     result = []
     for tags in str:
         if re.match(u"[a-zA-Z\d\u4e00-\u9fa5]", tags):  # 只将tags中的英文、数字、汉字保存进result中
@@ -32,11 +32,11 @@ def filter(str):
 # 计算文章相似度
 def calc_similarity(text1, text2):
     texts = [text1, text2]
-    dictionary = gensim.corpora.Dictionary(texts)
-    corpus = [dictionary.doc2bow(text) for text in texts]
-    similarity = gensim.similarities.Similarity('-Similarity-index', corpus, num_features=len(dictionary))
-    test_corpus = dictionary.doc2bow(text1)
-    cosine_sim = similarity[test_corpus][1]
+    dictionary = gensim.corpora.Dictionary(texts)  # 根据两篇文章的所有内容构建词表
+    corpus = [dictionary.doc2bow(text) for text in texts]  # 记录词表中所有词的频率
+    similarity = gensim.similarities.Similarity(None, corpus=corpus, num_features=len(dictionary))  # 参数为：碎片文件名路径(不管,在缓存中随机读取）、词频、字典大小
+    test_corpus = dictionary.doc2bow(text1)  # 文章一中所有词在词表中的出现频率
+    cosine_sim = similarity[test_corpus][1]  # 对文章一进行相似度查询
     return cosine_sim
 
 
@@ -51,5 +51,5 @@ if __name__ == '__main__':
     text_fix = filter(str_fix)
     similarity = calc_similarity(text_ori, text_fix)
     f = open(path_answer, 'w')
-    f.write('文章相似度:{0:.4f}'.format(similarity))
+    f.write('文章相似度:{0:.6f}'.format(similarity))
     f.close()
