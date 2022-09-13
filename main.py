be0784af01afd13ce17ca04d576686e7dffd9dd9
摸鱼AI衔尾蛇 <113227682+SnakeSwallowingTail@users.noreply.github.com>
2022-09-13 17:28:01 +0800

    优化函数命名、进行部分单元测试

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ def get_content(path):
 
 
 # jieba分词并过滤标点符号等
-def filter(str):
+def char_filter(str):
     str = jieba.lcut(str)  # jieba分词，直接返回list类型
     result = []
     for tags in str:
-        if re.match(u"[a-zA-Z\d\u4e00-\u9fa5]", tags):  # 只将tags中的英文、数字、汉字保存进result中
+        if re.match(u'[a-zA-Z\d\u4e00-\u9fa5]', tags):  # 只将tags中的英文、数字、汉字保存进result中
             result.append(tags)
         else:
             pass
@@ -34,9 +34,9 @@ def calc_similarity(text1, text2):
     texts = [text1, text2]
     dictionary = gensim.corpora.Dictionary(texts)  # 根据两篇文章的所有内容构建词表
     corpus = [dictionary.doc2bow(text) for text in texts]  # 记录词表中所有词的频率
-    similarity = gensim.similarities.Similarity(None, corpus=corpus, num_features=len(dictionary))  # 参数为：碎片文件名路径(不管,在缓存中随机读取）、词频、字典大小
+    similarities = gensim.similarities.Similarity(None, corpus=corpus, num_features=len(dictionary))  # 参数为：碎片文件名路径(不管,在缓存中随机读取）、词频、字典大小
     test_corpus = dictionary.doc2bow(text1)  # 文章一中所有词在词表中的出现频率
-    cosine_sim = similarity[test_corpus][1]  # 对文章一进行相似度查询
+    cosine_sim = similarities[test_corpus][1]  # 对文章一进行相似度查询
     cosine_sim = float("%.6f" % cosine_sim)
     return cosine_sim
 
@@ -48,9 +48,9 @@ if __name__ == '__main__':
     path_answer = 'D:\Courses\SE\PaperPassSystem-3120004933/ans.txt'
     str_ori = get_content(path_original)
     str_fix = get_content(path_fixed)
-    text_ori = filter(str_ori)
-    text_fix = filter(str_fix)
+    text_ori = char_filter(str_ori)
+    text_fix = char_filter(str_fix)
     similarity = calc_similarity(text_ori, text_fix)
-    f = open(path_answer, 'w')
-    f.write('文章相似度:{0:.6f}'.format(similarity))
-    f.close()
+    fout = open(path_answer, 'w')
+    fout.write('文章相似度:{0:.6f}'.format(similarity))
+    fout.close()
